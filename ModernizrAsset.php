9219dda15e20fdfc860bf5685f3b01c67dc24f14
Grzegorz Kurtyka <grzegorz.kurtyka@schibsted.pl>
2015-06-12 09:54:09 +0200

    Initial commit

---
+++ ModernizrAsset.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Modernizr Asset
+ * @author Grzegorz Kurtyka <grzegorz.kurtyka@gmail.com>
+ */
+
+namespace grzegorzkurtyka\modernizr;
+
+use yii\web\AssetBundle;
+
+class ModernizrAsset extends AssetBundle {
+
+    public $sourcePath = '@bower/modernizr';
+    public $js = ['modernizr.js'];
+    public $jsOptions[
+      'position' => View::POS_BEGIN
+    ];
+}
